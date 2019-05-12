8d07ef5de68127e5feff436f1f5ba8f818f89216
Weier Ni <wni007@ucr.edu>
2019-05-11 18:56:55 -0700

    fix

--- unit_tests/test.cpp
+++ unit_tests/test.cpp
@@ -1,62 +1,62 @@
 #include "gtest/gtest.h"
-#include "connector.h"
-#include "command.h"
-#include " parse.h"
-#include "and.h'
-#include "or.h"
-#include "semicolonm.h"
-#include "base.h"
+#include "../src/connector.h"
+#include "../src/command.h"
+#include "../src/parse.h"
+#include "../src/and.h"
+#include "../src/or.h"
+#include "../src/semicolon.h"
+#include "../src/base.h"
+
+TEST(Connector_tests, And_correct1)
+{
+	Base* left = new Command("ls -a");
+	Base* right = new Command("echo hello world");
+	Base* test = new And(left,right);
+	EXPECT_EQ(1,test->execute());
+}
+
+TEST(Connector_tests, And_correct2)
+{	
+	Base* left = new Command("ls");
+	Base* right = new Command("echo hello world");
+	Base* test = new And(left,right);
+	EXPECT_EQ(1,test-> execute());
+}
+
+TEST(Connector_tests,Or_correct1)
+{
+	Base* left = new Command("echo hello");
+	Base* right = new Command("mkdir test");
+	Base* test  = new Or(left,right);
+	EXPECT_EQ(1,test-> execute());
+}
+
+TEST(Connector_tests,Or_correct2)
+{
+	Base* left = new Command(" ls");
+	Base* right = new Command("ls");
+	Base* test = new Or(left, right);
+	EXPECT_EQ(1, test-> execute());
+}
+
+TEST(Connector_tests,Semicolon_correct1)
+{
+	Base* left = new Command("echo hi");
+	Base* right = new Command("echo world");
+	Base* test = new Semicolon(left, right);
+	EXPECT_EQ(1,test->execute());
+}
+
+TEST(Connector_tests,Semicolon_correct2)
+{
+	Base* left = new Command("echo ls");
+	Base* right = new Command("echo fast");
+	Base* test = new Semicolon(left,right);
+	EXPECT_EQ(1,test->execute());
+}
 
 int main ( int argc, char ** argv)
 {
 	::testing::InitGoogleTest(&argc,argv);
-	return RUN_ALL_TEST():
+	return RUN_ALL_TESTS();
 }
-Test(Connector_tests, And_correct1)
-{
-	Command* left = new Command("ls -a");
-	Command* right = new Command("echo hello world");
-	And* test = new And(left,right);
-	EXPECT_EQ(1,test->execute());
-}
-
-Test(Connector_tests, And_correct2)
-{	
-	Command* left = new Command("ls");
-	Command* right = new Command("echo hello world");
-	And* test = new And(left,right);
-	EXPECT_EQ(1,test-> execute());
-}
-
-Test(Connector_tests,Or_correct1)
-{
-	Command* left = new Command("echo hello");
-	Command* right = new Command("mkdir test");
-	Or* test  = new Or(left,right);
-	EXPECT_EQ(1,test-> execute());
-}
-
-Test(Connector_tests,Or_correct2)
-{
-	Command* left = new Command(" ls");
-	Command* right = new Command("ls");
-	Or* test = new Or(left, right);
-	EXPECT_EQ(1, test-> execute());
-}
-
-Test(Connector_tests,Semicolon_correct1)
-{
-	Command* left = new Command("echo hi");
-	Command* right = new Command("echo world");
-	Semicolon* test = new Semicolon(left, right);
-	EXPECT_EQ(1,test->execute()):
-}
-
-Test(Connector_tests,Semicolon_correct2)
-{
-	Command* left = new Command("echo ls");
-	Command* right = new Command("echo fast");
-	Semicolon* test = new Semicolon(left,right);
-	EXPECT_EQ(1,TEST->EXECUTE());
-}
-
