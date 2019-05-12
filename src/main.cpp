3130b1ca5773f341c838a9ac982fb91614800342
Weier Ni <wni007@ucr.edu>
2019-05-11 19:18:50 -0700

    $

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <command.h>
+#include <connector.h>
+#include <parse.h>
+#include <and.h>
+#include <or.h>
+#include <semicolon.h>
+#include <base.h>
+
+int main(){
+	std::cout << "$ ";
+}
