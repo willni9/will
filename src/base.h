1afd0b2605bfaaa0b891f7b3e5fb9406434d14d9
jianbin li <jli360@ucr.edu>
2019-05-11 17:33:06 -0700

    new base

--- src/base.h
+++ src/base.h
@@ -8,13 +8,15 @@
 using namespace std;
 
 class Base{
+	private : 
+
 	public:
 		Base(){};
-		
+		virtual void setBase(Base*, Base*) = 0;
 		virtual bool execute() = 0;
 		virtual string get_cmd() = 0;
 		virtual bool isConnector() = 0;
 		
-}
+};
 
 #endif
