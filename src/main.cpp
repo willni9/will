427298c2e9cc633d5d8a89eb1e1bdd44e62aa5d6
Weier Ni <wni007@ucr.edu>
2019-05-11 19:54:56 -0700

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <command.h>
-#include <connector.h>
-#include <parse.h>
-#include <and.h>
-#include <or.h>
-#include <semicolon.h>
-#include <base.h>
+#include "command.h"
+#include "connector.h"
+#include "parse.h"
+#include "and.h"
+#include "or.h"
+#include "semicolon.h"
+#include "base.h"
 
 int main(){
 	std::cout << "$ ";
+	return 0;
 }
